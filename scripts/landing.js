f26a7c4a529f6bd0ade495bdbf5d172f808f08a6
Sean Bode <seanpaulbode@gmail.com>
2016-11-16 16:46:14 -0500

    c24-dom-scripting-collection

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,12 @@
-var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
 
-  var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+var animatePoints = function (points) {
+	var revealPoint = function (index) {
+		points[index].style.opacity = 1;
+		points[index].style.transform = "scaleX(1) translateY(0)";
+		points[index].style.msTransform = "scaleX(1) translateY(0)";
+		points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+	};
 
   for (var i = 0; i < points.length; i ++) {
     revealPoint();
@@ -15,6 +14,8 @@ var animatePoints = function(points) {
 };
 
 window.onload = function() {
+	var pointsArray = document.getElementsByClassName('point');
+	
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
 }
